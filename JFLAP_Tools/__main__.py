effc049c43db5df3ee44e26d1d8b6b4960cc707a
Jake Hoffman <jaketh19@gmail.com>
2023-02-06 23:38:25 -0500

    Added documentation

--- JFLAP_Tools/__main__.py
+++ JFLAP_Tools/__main__.py
@@ -8,7 +8,21 @@ def createParser()->argparse.ArgumentParser:
 
     parser = argparse.ArgumentParser(
         prog='REToJFLAPFA',
-        description='Convert a regular expression to a JFLAP Finite Automaton')
+        description="""Convert a regular expression to a JFLAP Finite Automaton.
+        
+Sets are designated using square brackets, such as '[abcd]' to represent the transition containing each 'a', 'b', 'c', and 'd'.
+Sets can also contain a range of values from 'a' to '9' by using a '-'. 
+    Ex. "[a-d]" for 'a', 'b', 'c', 'd' or "[a-cZ-1]" for 'a', 'b', 'c', 'Z', '0', '1'
+
+Parenthesis can be used to designate a group for repeating.
+    Ex. "(abc)*" represents '', 'abc', 'abcabc', ...
+
+Pipe is used to represent an OR operation (lambda transition).
+    Ex. "a*|[bc]" represent 'a', 'aa', 'aaa', ... , 'b', or 'c'
+
+Nested repeaters, such as "(ab*cd)*", are not currently supported.""",
+        epilog='Not providing an outfile will output to terminal',
+        formatter_class=argparse.RawTextHelpFormatter)
     
     parser.add_argument('regex')
     parser.add_argument('-o', '--outfile')
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # RegEx to JFLAP Finite Automaton
 
-Current Bug:
-"a|b*"
+A command line tool for converting a simple Regular Expression into a finite automaton
 
-issue is with '|' followed by single character match many
+For information on how to use the commnad line tool, use ```-h``` or ```--help```.
